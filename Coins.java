0c90a5e58952ff30ec2f5a9dc836a5d2b1c04402
noa apteker <noaapteker@h-MacBook-Air-sl-noa.local>
2023-12-23 13:48:58 +0200

    HW1

--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int totalMoney = Integer.parseInt(args[0]);
+		int quarters = totalMoney / 25;
+		int cents = totalMoney % 25;
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,14 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int randomnum1 = (int)(Math.random()*(b-a))+a;
+		int randomnum2 = (int)(Math.random()*(b-a))+a;
+		int randomnum3 = (int)(Math.random()*(b-a))+a;
+		System.out.println(randomnum1);
+		System.out.println(randomnum2);
+		System.out.println(randomnum3);
+		System.out.println("The minimal generated number was " + Math.min(Math.min(randomnum1, randomnum2), randomnum3));
 	}
 }
